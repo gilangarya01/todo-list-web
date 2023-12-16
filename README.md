2b957eba0d9c637fc84b1c6e6121d26f6feeb02b
garpra27 <gilangapradana01@gmail.com>
2023-12-16 11:26:18 +0700

    membuat ui untuk todolist

---
+++ README.md
@@ -0,0 +1,10 @@
+## To-Do List
+
+Selamat datang di proyek To-Do List, sebuah aplikasi web yang dirancang untuk membantu Anda mengelola tugas sehari-hari dengan mudah dan efisien. Dengan desain yang bersih dan antarmuka yang ramah pengguna, proyek ini memanfaatkan teknologi HTML, Tailwind CSS, JavaScript, dan DaisyUI untuk memberikan pengalaman yang intuitif dan responsif.
+
+##### Teknologi
+
+- HTML
+- JavaScript
+- Tailwind CSS
+- DaisyUI
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require("tailwindcss/colors");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./*.html", "./pages/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: colors.black,
+        white: colors.white,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        yellow: colors.yellow,
+        stone: colors.stone,
+        sky: colors.sky,
+        neutral: colors.neutral,
+        gray: colors.gray,
+        slate: colors.slate,
+        lime: colors.lime,
+        rose: colors.rose,
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
